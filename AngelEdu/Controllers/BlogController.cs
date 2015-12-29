af0431c29093baefbc4c36c6a8a95e62d97a2285
monsoonq <jifeng.qian@gmail.com>
2015-12-28 17:35:03 -0800

    add AngelEdu

---
+++ AngelEdu/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+
+namespace AngelEdu.Controllers
+{
+    
+    
+    public class BlogController : Controller
+    {
+        
+        public ActionResult Home1()
+        {
+            return View();
+        }
+    }
+}
